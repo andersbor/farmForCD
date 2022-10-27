9af8eed20ca78be907ebea44eb3541fd8533290b
Anders Børjesson <anbo@zealand.dk>
2022-10-27 10:01:30 +0200

    Cow class and unit test

---
+++ farmForCD/Models/Cow.cs
@@ -0,0 +1,27 @@
+﻿namespace farmForCD.Models
+{
+    public class Cow
+    {
+        public int MyProperty { get; set; }
+        public string Name { get; set; }
+        public int Weight { get; set; }
+
+        public void ValidateName()
+        {
+            if (Name == null)
+            {
+                throw new ArgumentNullException("Name cannot be null");
+            }
+            if (Name.Length < 3)
+            {
+                throw new ArgumentException("Name must be at least 3 characters long: " + Name);
+            }
+        }
+
+        override
+        public string ToString()
+        {
+            return $"Cow: {Name} weighs {Weight} pounds.";
+        }
+    }
+}
